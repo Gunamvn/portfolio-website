42bd7cb2234436d835fcbcfaca7647fdca91301c
Gunamvn <55233502+Gunamvn@users.noreply.github.com>
2025-01-19 23:08:34 -0700

    Removed PDF Preview

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,14 +4,7 @@ import '../assets/css/styles.css';
 const Resume = () => {
     return (
         <div className="resume-container">
-            {/* PDF Preview */}
-            <iframe
-                src="/resume.pdf"
-                width="100%"
-                height="600px"
-                title="Resume Preview"
-                className="resume-iframe"
-            ></iframe>
+            
             <p>
             <a
                 href="/resume.pdf"
